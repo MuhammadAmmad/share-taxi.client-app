e89be671c5a7fb24da8ed4a4e80a1e3c0aee01f3
naama <naama@macbook-pros-MacBook-Pro.local>
2014-04-15 13:30:40 +0300

    +working gui Assaf+Naama's part, changed the manifest

--- sharetaxi/src/com/panamana/sharetaxi/activities/MapActivity.java
+++ sharetaxi/src/com/panamana/sharetaxi/activities/MapActivity.java
@@ -1,8 +1,12 @@
 package com.panamana.sharetaxi.activities;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.PolylineOptions;
@@ -14,7 +18,7 @@ import com.panamana.sharetaxi.maps.Maps;
  * Main Activity.
  * @author 
  */
-public class MapActivity extends Activity {
+public class MapActivity extends ActionBarActivity {
 
 	/**
 	 * 
@@ -39,6 +43,16 @@ public class MapActivity extends Activity {
 		Directions.drawRoute(pointA,pointB,waypoints);
 	}
 	
+	/*
+	 * create the menu
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.map_menu, menu);
+	    return true;
+	}
+	
 	/**
 	 * 
 	 * @param lineOptions
@@ -48,5 +62,52 @@ public class MapActivity extends Activity {
 		lineOptions.width(5);
 		Maps.drawPolyline(lineOptions);// TODO Auto-generated method stub
 	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle item selection
+	    switch (item.getItemId()) {
+        case R.id.map_menu_item0:
+            item0Clicked();
+
+	    case R.id.map_menu_item1:
+	            item1Clicked();
+	            return true;
+	        case R.id.map_menu_item2:
+	            item2Clicked();
+	            return true;
+	        case R.id.map_menu_item3:
+	            item3Clicked();
+	            return true;
+	        case R.id.map_menu_item4:
+	            item4Clicked();
+	            return true;
+
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+	
+	private void item0Clicked() {
+		startActivity(new Intent(this, MainActivity.class));
+	}
+
+	private void item1Clicked() {
+		startActivity(new Intent(this, Kav4Activity.class));
+	}
+
+	private void item2Clicked() {
+		startActivity(new Intent(this, Kav4_A_Activity.class));
+	}
+
+	private void item3Clicked() {
+		startActivity(new Intent(this,Kav5Activity.class));
+	}
+
+	private void item4Clicked() {
+		startActivity(new Intent(this, ResultsActivity.class));
+	}
+
+	
 }
 
